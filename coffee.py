cad3f0b0ba1f9e760408e3d03c8318cfbdeeb252
AnAka10 <an621@kent.ac.uk>
2026-02-02 13:58:24 +0000

    Part 2 Workshop 5

--- coffee.py
+++ coffee.py
@@ -1,1 +1,8 @@
-#Your code goes here
+amount_due = 50
+
+while amount_due > 0:
+    print("Amount Due:", amount_due)
+    coin = int(input("insert Coin:  "))
+    if coin in [50, 20, 20, 5]:
+        amount_due -= coin
+        print ("Changed owned:", abs(amount_due))
