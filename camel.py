230a478744af0eda4374e404ebdb8996d143eba0
AnAka10 <an621@kent.ac.uk>
2026-02-02 13:49:46 +0000

    Task 1 Workshop 5

--- camel.py
+++ camel.py
@@ -1,1 +1,8 @@
-
+Name = input ("CamelCase: ")
+Snake_Case = ""
+for char in Name:
+    if char.isupper():
+        Snake_Case += "_" + char.lower()
+    else:
+        Snake_Case += char
+print ("Snake_Case:", Snake_Case)
